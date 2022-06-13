86865f9c721eefbafa1dfde053e4788365dfdc60
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-06-13 19:57:21 -0400

    sending index and show data

---
+++ server.js
@@ -0,0 +1,33 @@
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+console.log(PORT)
+
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+  })
+  
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('nomming at port', PORT);
+})
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+ 
+
