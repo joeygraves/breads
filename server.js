f6e88ea0ce619bab6806782d136654e5f583c1eb
Joseph Graves <jcg97@Josephs-MacBook-Pro.local>
2022-06-25 16:38:05 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Check Back for Pt 4
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -15,6 +16,11 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -26,11 +32,10 @@ app.get('*', (req, res) => {
   res.send('404')
 })
 
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
 })
   
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+
